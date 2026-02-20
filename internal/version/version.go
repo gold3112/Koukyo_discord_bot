c7bec22f47331392af934acd4e6464206bceac81
gold <tomo.soga50@gmail.com>
2026-02-20 23:28:48 +0900

    パッチノート更新v2.0.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// Botのバージョン番号
-	Version = "1.9.4"
+	Version = "2.0.0"
 
 	// SupportServerURL サポートサーバーのURL
 	SupportServerURL = "https://discord.gg/AgzmhFk43Z"
@@ -10,11 +10,6 @@ const (
 
 // PatchNotes パッチノートの内容
 var PatchNotes = []string{
+	"endpoint利用の最適化のために、ユーザー断定ロジックを大幅に変更(めっちゃ速い)",
 	"10px以下の荒らしの通知を控えめなものに変更",
-	"↓に関連して、ピクセルアート進捗監視機能を追加",
-	"追加監視機能(京都御所、松代大本営等)を実装",
-	"getコマンドのショートカット追加",
-	"ロジックの効率化とバグ修正",
-	"regionmap,meコマンドの実装",
-	"--現在移植中!!!--",
 }
